5d613e00c8916ae51c3863098461cb9a6b914c0f
rcarlosdev <carl.rest19@gmail.com>
2021-11-01 00:39:33 -0500

    formulario de contacto creado

--- src/app/settings.py
+++ src/app/settings.py
@@ -21,7 +21,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
+    'core',
 ]
+
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+NOTITY_EMAIL = env('NOTITY_EMAIL')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -83,23 +90,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render, reverse
 from django.views import generic
+from .forms import ContactForm
+from django.conf import settings
+from django.contrib import messages
 
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
+
+
+class ContactView(generic.FormView):
+    form_class = ContactForm
+    template_name = 'contact.html'
+
+    def get_success_url(self):
+        return reverse('contact')
+
+    def form_valid(self, form):
+        messages.info(
+            self.request,
+            'Hemos recibido tu mensaje.'
+        )
+        name = form.cleaned_data.get('name')
+        email = form.cleaned_data.get('email')
+        message = form.cleaned_data.get('message')
+
+        # Enviar el correo electrónico aquí.
+        full_message = f"""
+            Mesaje recibido de {name}, {email}
+            --------------------------------------------------------------------
+
+            {message}
+        """
+        send_mail(
+            subject='Mensaje recibido por Contact Form',
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.NOTITY_EMAIL]
+        )
+        return super(ContactView, self).form_valid(form)
